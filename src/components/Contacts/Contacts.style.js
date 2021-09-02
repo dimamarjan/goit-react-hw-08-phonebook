c8c3466d11ab9c86d18354bcc002e1bb8e4873ee
dimamarjan <dima.marjan@gmail.com>
2021-09-02 18:02:25 +0300

    fix

--- src/components/Contacts/Contacts.style.js
+++ src/components/Contacts/Contacts.style.js
@@ -5,7 +5,7 @@ export const ContactsMainSection = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  min-height: calc(100vh - 70px);
+  min-height: calc(100vh - 90px);
   background-color: rgb(33, 33, 33);
   padding-left: 150px;
   padding-right: 150px;
